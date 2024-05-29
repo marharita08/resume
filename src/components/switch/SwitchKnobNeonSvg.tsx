666f04e24abd56388181cb1405a230091984fae8
marharita08 <rozgon.margarita08@gmail.com>
2024-05-29 13:10:42 +0300

    fix property names to camel case

--- src/components/switch/SwitchKnobNeonSvg.tsx
+++ src/components/switch/SwitchKnobNeonSvg.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export const SwitchKnobNeonSvg = () => {
   return (
     <svg className="switch__knob-neon" viewBox="0 0 48 48" width="38px" height="38px">
-      <circle fill="none" stroke="url(#switch-gradient2)" stroke-dasharray="0 90.32 0 54.19" stroke-linecap="round"
-              stroke-width="1" r="23" cx="24" cy="24" transform="rotate(-112.5,24,24)"/>
+      <circle fill="none" stroke="url(#switch-gradient2)" strokeDasharray="0 90.32 0 54.19" strokeLinecap="round"
+              strokeWidth="1" r="23" cx="24" cy="24" transform="rotate(-112.5,24,24)"/>
     </svg>
   )
 }
--- src/components/switch/SwitchNeonBaseSvg.tsx
+++ src/components/switch/SwitchNeonBaseSvg.tsx
@@ -12,17 +12,17 @@ export const SwitchNeonBaseSvg = () => {
           </feMerge>
         </filter>
         <linearGradient id="switch-gradient1" x1="0" y1="0" x2="1" y2="0">
-          <stop offset="0%" stop-color="hsl(var(--on-hue1),90%,70%)"/>
-          <stop offset="100%" stop-color="hsl(var(--on-hue2),90%,70%)"/>
+          <stop offset="0%" stopColor="hsl(var(--on-hue1),90%,70%)"/>
+          <stop offset="100%" stopColor="hsl(var(--on-hue2),90%,70%)"/>
         </linearGradient>
         <linearGradient id="switch-gradient2" x1="0.7" y1="0" x2="0.3" y2="1">
-          <stop offset="25%" stop-color="hsla(var(--on-hue1),90%,70%,0)"/>
-          <stop offset="50%" stop-color="hsla(var(--on-hue1),90%,70%,0.3)"/>
-          <stop offset="100%" stop-color="hsla(var(--on-hue2),90%,70%,0.3)"/>
+          <stop offset="25%" stopColor="hsla(var(--on-hue1),90%,70%,0)"/>
+          <stop offset="50%" stopColor="hsla(var(--on-hue1),90%,70%,0.3)"/>
+          <stop offset="100%" stopColor="hsla(var(--on-hue2),90%,70%,0.3)"/>
         </linearGradient>
       </defs>
-      <path fill="none" filter="url(#switch-glow)" stroke="url(#switch-gradient1)" stroke-width="1"
-            stroke-dasharray="0 104.26 0" stroke-dashoffset="0.01" stroke-linecap="round"
+      <path fill="none" filter="url(#switch-glow)" stroke="url(#switch-gradient1)" strokeWidth="1"
+            strokeDasharray="0 104.26 0" strokeDashoffset="0.01" strokeLinecap="round"
             d="m.5,12C.5,5.649,5.649.5,12,.5h16c6.351,0,11.5,5.149,11.5,11.5s-5.149,11.5-11.5,11.5H12C5.649,23.5.5,18.351.5,12Z"/>
     </svg>
   )
