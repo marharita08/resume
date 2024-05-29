4e808de25a2e1a195b313e81e84140a082c0c75f
marharita08 <rozgon.margarita08@gmail.com>
2024-05-29 13:53:18 +0300

    update data

--- src/components/main/summary/ItemsData.tsx
+++ src/components/main/summary/ItemsData.tsx
@@ -2,9 +2,11 @@ import {SubItem} from "../SubItemTypes";
 
 export const items: SubItem[] = [
   {
-    description: "I am a master's student in Computer Science at Sumy State University. I have a Bachelor's degree in the same field." +
-      "I studied web development at the university and took additional courses." +
-      "I enjoy development. Working with code brings me joy and fulfillment." +
-      "I have a desire to improve my skills as a developer and gain experience working in a team."
+    description:
+    "I hold a Master's and a Bachelor's degree in Computer Science, both obtained from Sumy State University. " +
+      "I am interested in web development. I started studying it at university, continued learning through courses, and am still learning independently. " +
+      "I enjoy development. Working with code brings me joy and fulfillment. " +
+      "I have a desire to improve my skills as a developer and gain experience working in a team. " +
+      "I am open to new opportunities and challenges."
   }
 ];
