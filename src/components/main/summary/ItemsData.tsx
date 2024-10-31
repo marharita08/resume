359232330b9d91684d663d8daa36453325bc6017
marharita08 <rozgon.margarita08@gmail.com>
2024-10-31 11:19:09 +0200

    fix typo in abstract

--- src/components/main/summary/ItemsData.tsx
+++ src/components/main/summary/ItemsData.tsx
@@ -5,7 +5,7 @@ export const items: SubItem[] = [
     description:
       "I hold a Master's and a Bachelor's degree in Computer Science, both obtained from Sumy State University. " +
       "Studing at university I got interested in web development. " +
-      "So, I also fonished 'MindK Dev Camp' and JS course at Binary Studio Academy. " +
+      "So, I also finished 'MindK Dev Camp' and JS course at Binary Studio Academy. " +
       "As part of JS course, I worked together with other students on development of BeBalance application, where I worked as Full-Stack Developer. " +
       "I enjoy development.  I have a strong desire to find my first job as a developer to continue growing professionally. " +
       "I am open to new opportunities and challenges."
